935671d56f747361b2c808a3649ed68f426a0b65
Wendy Fgigueroa <camomila444@gmail>
2021-01-09 22:19:13 -0500

    changing layout in html

--- assets/script.js
+++ assets/script.js
@@ -15,3 +15,15 @@
 //       prevEl: '.swiper-button-prev',
 //     },
 //   });
+
+/*--------callout middle--------*/
+
+$(function(){
+  $('[data-callout-hover-reveal]').hover(function(){
+    $(this).find('.callout-footer').slideDown(250);
+      },function(){
+    $(this).find('.callout-footer').slideUp(250);
+    });
+  })
+  
+  /***-----------------------new card--------------------------- */
